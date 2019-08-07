03fdaafd1496ec2a110eb1508bdf7b1940945e4e
amirgc <gcamir15@gmail.com>
2019-08-07 16:55:40 -0400

    3 files added

---
+++ src/app/component-templates/component-templates.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LifeCycleHooksComponent } from './life-cycle-hooks/life-cycle-hooks.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LifeCycleHooksComponent]
+})
+export class ComponentTemplatesModule { }
